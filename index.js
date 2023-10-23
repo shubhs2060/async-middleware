e72087dc12ef4c1f02b2ec6960de2b40bafadac7
shubham <shubhamsrivastava2050@gmail.com>
2023-10-23 18:43:42 +0530

    add middleware

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    asyncMiddleware: function(handler){
+        return async (req, res, next) => {
+            try{
+                await handler(req, res);
+            }catch(error){
+                next(error)
+            }
+        }
+    }
+    
+}
