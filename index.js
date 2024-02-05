f180018d39d6e2f191b0f041f2e534e0598e8ee7
shubhs2060 <36035437+shubhs2060@users.noreply.github.com>
2024-02-05 14:46:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { IncomingWebhook } = require('ms-teams-webhook');
+
 module.exports = {
 
     asyncMiddleware: function(handler){
@@ -8,6 +10,45 @@ module.exports = {
                 next(error)
             }
         }
-    }
+    },
+
+    asyncTeamsErrorMiddleware: function(handler,options){
+        return async (req,res,next) => {
+            try {
+                await handler(req,res);
+            } catch (error) {
+                const webhook = new IncomingWebhook(options.MS_TEAM_WEBHOOK_URL);
+                if(req.session && req.session.user != undefined){
+                    var message = `${error.message} for path ${req.originalUrl}` + " " + req.session.user.Email
+                }else{
+                    var message = `${error.message} for path ${req.originalUrl}`
+                }
+                let body = null;
+                if(req.body){
+                    body = `${JSON.stringify(req.body)}`
+                }else if(req.body){
+                    body = `${JSON.stringify(req.body)}`
+                }else{
+                    body = `Params ${req.params} Query ${req.query}`
+                }
+                await webhook.send({
+                    "@type": "MessageCard",
+                    "@context": "https://schema.org/extensions",
+                    "summary": `${ req.error.message || "Async Teams Error"}`,
+                    "themeColor": "0078D7",
+                    "title": message,
+                    "sections": [
+                        {
+                        "activityTitle": "Async Teams Error",
+                        "activitySubtitle": `${ options.NODE_ENV || "dev"}`,
+                        "activityImage": "https://cdn-icons-png.flaticon.com/512/2103/2103533.png",
+                        "text": body
+                        }
+                    ]
+                })
+                next(error)
+            }
+        }
+    },
     
 }
